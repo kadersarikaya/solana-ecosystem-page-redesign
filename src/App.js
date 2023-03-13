65ce7b84ad8bcd38b777b2ac6816ae2d59bc3f41
unknown <kadersarikaya0123@gmail.com>
2023-03-13 17:49:27 +0300

    hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Solnavbar} from './components';
+import {Header} from './containers';
 
 function App() {
   return (
     <div className="App">
       <Solnavbar/>
+      <Header/>
     </div>
   );
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import './header.css';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='sol__header main-padding'>
+      <div className="sol__header-content">
+        <div className="sol__header-content-cta">
+          <h1>Discover the hottest projects on Solana</h1>
+          <button>EXPLORE THE ECOSYSTEM</button>
+        </div>
+        <div className="sol__header-image">
+        </div>
+      </div>
+    </div>
   )
 }
 
